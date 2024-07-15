8c1f0c83f844b8dbee12a6e6edf6dd74c00e69c7
Lgsarius <33753599+Lgsarius@users.noreply.github.com>
2024-07-15 17:54:53 +0200

    feat: Remove next-sitemap.js configuration file
    The commit removes the `next-sitemap.js` configuration file, which was no longer needed. This simplifies the project structure and removes unnecessary code.

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+// next-sitemap.config.js
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://www.lo-fi.study', // Replace with your site's URL
+  generateRobotsTxt: true, // (optional) Generate robots.txt file
+  // ...other options
+};
