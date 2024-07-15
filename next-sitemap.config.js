486fcb59e3d757f6fecba722814501b8bc661881
Lgsarius <33753599+Lgsarius@users.noreply.github.com>
2024-07-15 17:57:48 +0200

    feat: Update next-sitemap.config.js for improved SEO and sitemap generation
    The commit updates the next-sitemap.config.js file to include additional paths for the sitemap, such as the login and study pages. It also adds changefreq and priority options to specify the frequency of page changes and their importance. Additionally, the commit updates the robots.txtOptions to allow all user agents to access the site. These changes improve the SEO and sitemap generation of the website.

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,30 @@
-// next-sitemap.config.js
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://www.lo-fi.study', // Replace with your site's URL
-  generateRobotsTxt: true, // (optional) Generate robots.txt file
-  // ...other options
+  siteUrl: 'https://www.lo-fi.study',
+  generateRobotsTxt: true, // Generate robots.txt file
+  changefreq: 'daily', // How frequently the page is likely to change
+  priority: 1.0, // Priority of the main page
+  sitemapSize: 5000, // Maximum entries per sitemap file
+  additionalPaths: async (config) => [
+    {
+      loc: '/', // This will be https://www.lo-fi.study/
+      changefreq: 'daily',
+      priority: 1.0,
+    },
+    {
+      loc: '/login', // This will be https://www.lo-fi.study/login
+      changefreq: 'daily',
+      priority: 0.8,
+    },
+    {
+      loc: '/study', // This will be https://www.lo-fi.study/study
+      changefreq: 'daily',
+      priority: 0.8,
+    },
+  ],
+  robotsTxtOptions: {
+    policies: [
+      { userAgent: '*', allow: '/' },
+    ],
+  },
 };
