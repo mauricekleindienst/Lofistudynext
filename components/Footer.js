14b03212a9deac608d3ea9fbd60ecb4789ce93e7
Maurice Kleindienst <support@mousewerk.de>
2024-07-18 12:02:55 +0200

    feat: Update Footer component with new content and styles
    This commit updates the Footer component by replacing the existing content and styles. The new content includes updated copyright information and links to legal, data policy, and contact pages. The styles have been adjusted to match the overall design of the application. This update ensures that the Footer component is up to date and visually consistent with the rest of the app.

--- components/Footer.js
+++ components/Footer.js
@@ -1,51 +1,27 @@
-// components/Footer.js
 import Link from 'next/link';
-import Image from 'next/image';
-import styles from '../styles/Home.module.css';
+import styles from './Footer.module.css'; // Adjust the path as necessary
 
-const Footer = () => {
-    return (
-        <div className={`row ${styles.rowWithLine}`}>
-            <div className={`container py-3 ${styles.footerWithLine}`}>
-                <div className={`footer-content d-flex justify-content-between align-items-center ${styles.footerContent}`}>
-                    <div className={`footer-left ${styles.footerLeft}`}>
-                        <div className={`copyright py-2 ${styles.copyright}`}>
-                            <Image className={`logo-shadow ${styles.reducedSize}`} src="/favicon.ico" alt="Logo" width={50} height={50} />
-                            <p className="text-white">Copyright © 2024 lo-fi.study</p>
-                        </div>
-                    </div>
-                    <div className={`footer-middle ${styles.footerMiddle}`}>
-                        <div className={`settings-item py-2 ${styles.settingsItem}`}>
-                            <Link href="/legal_notice" passHref>
-                                <a className={`settings-link text-white ${styles.settingsLink}`}>Legal Notice
-                            </Link>
-                        </div>
-                        <div className={`settings-item py-2 ${styles.settingsItem}`}>
-                            <Link href="/privacy_policy" passHref>
-                                <a className={`settings-link text-white ${styles.settingsLink}`}>Privacy Policy
-                            </Link>
-                        </div>
-                    </div>
-                    <div className={`footer-right ${styles.footerRight}`}>
-                        <div className={`settings-item py-2 ${styles.settingsItem}`}>
-                            <Link href="https://www.instagram.com/your_instagram_handle" passHref>
-                                <a className={`settings-link text-white ${styles.settingsLink}`}>
-                                    <i className="fab fa-instagram fa-3x"></i>
-                                
-                            </Link>
-                        </div>
-                        <div className={`settings-item py-2 ${styles.settingsItem}`}>
-                            <Link href="https://twitter.com/lo_fi_study" passHref>
-                                <a className={`settings-link text-white ${styles.settingsLink}`}>
-                                    <i className="fab fa-x-twitter fa-3x"></i>
-                                
-                            </Link>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
-};
+const Footer = () => (
+    <footer className={styles.footer}>
+        <p>© 2024 Lo-Fi.Study App. All rights reserved.</p>
+        <ul>
+            <li>
+                <Link href="/legal">
+                    <a>Legal</a>
+                </Link>
+            </li>
+            <li>
+                <Link href="/data">
+                    <a>Data Policy</a>
+                </Link>
+            </li>
+            <li>
+                <Link href="/contact">
+                    <a>Contact</a>
+                </Link>
+            </li>
+        </ul>
+    </footer>
+);
 
 export default Footer;
