08696422c8df9955edff33186f3a3f11bf8143e6
Maurice Kleindienst <support@mousewerk.de>
2024-07-21 19:04:26 +0200

    chore: Add mobile-detect library for device detection in server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const http = require('http');
 const next = require('next');
 const { Server } = require('socket.io');
+const MobileDetect = require('mobile-detect');
 
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
@@ -16,24 +17,33 @@ app.prepare().then(() => {
 
   io.on('connection', (socket) => {
     console.log('a user connected');
-  
+
     // Send all stored messages to the new client
     socket.emit('chat history', messages);
-  
+
     socket.on('disconnect', () => {
       console.log('user disconnected');
     });
-  
+
     socket.on('chat message', (msg) => {
-      console.log("Received chat message:", msg);
+      console.log('Received chat message:', msg);
       messages.push(msg);
       io.emit('chat message', msg);
     });
   });
-  
 
   server.get('/hello', (req, res) => {
     return res.send('Hello World');
+  });
+
+  server.get('/app', (req, res) => {
+    const md = new MobileDetect(req.headers['user-agent']);
+
+    if (md.mobile()) {
+      res.redirect('/app_mobile');
+    } else {
+      handle(req, res);
+    }
   });
 
   server.all('*', (req, res) => {
