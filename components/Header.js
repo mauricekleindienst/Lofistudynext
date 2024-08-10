4d7f28e3146e03271efd041a23d951f548b879f5
Lgsarius <33753599+Lgsarius@users.noreply.github.com>
2024-08-10 15:50:25 +0200

    ```text chore: Update bootstrap dependency to version 5.3.3
    This commit updates the bootstrap dependency in the package.json and package-lock.json files to version 5.3.3. This update ensures that the application uses the latest stable version of Bootstrap, which provides bug fixes, performance improvements, and new features.

--- components/Header.js
+++ components/Header.js
@@ -1,39 +1,42 @@
+import { useState } from "react";
 import { useRouter } from "next/router";
 import styles from "../styles/Home.module.css";
-
 import Image from "next/image";
 
 export default function Header() {
   const router = useRouter();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const navigateTo = (path) => {
+    router.push(path);
+    setIsMenuOpen(false);
+  };
 
   return (
     <header className={styles.header}>
-      <div className={styles.logo} onClick={() => router.push("/")}>
-      <Image src="/lo-fi.study.svg" alt="lo-fi.study" width={100} height={100} />
+      <div className={styles.logo} onClick={() => navigateTo("/")}>
+        <Image src="/lo-fi.study.svg" alt="lo-fi.study" width={100} height={100} />
       </div>
-      <div className={styles.buttonContainer}>
-        <button
-          onClick={() => router.push("/dev-updates")}
-          className={styles.contactButton}
-        >
+      <div className={styles.hamburger} onClick={toggleMenu}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <div className={`${styles.buttonContainer} ${isMenuOpen ? styles.open : ''}`}>
+        <button onClick={() => navigateTo("/dev-updates")} className={styles.contactButton}>
           Updates
         </button>
-        <button
-          onClick={() => router.push("/Contact")}
-          className={styles.contactButton}
-        >
+        <button onClick={() => navigateTo("/Contact")} className={styles.contactButton}>
           Contact
         </button>
-        <button
-          onClick={() => router.push("/FAQ")}
-          className={styles.faqButton}
-        >
+        <button onClick={() => navigateTo("/FAQ")} className={styles.faqButton}>
           FAQ
         </button>
-        <button
-          onClick={() => router.push("/auth/signin")}
-          className={styles.loginButton}
-        >
+        <button onClick={() => navigateTo("/auth/signin")} className={styles.loginButton}>
           Sign In
         </button>
       </div>
