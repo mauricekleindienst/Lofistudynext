6f5417473bd7d0adcd0b850a5184c8f728bd6ab0
Maurice Kleindienst <support@mousewerk.de>
2024-10-22 09:57:05 +0200

    Refactor loader component to display dynamic messages during loading

--- pages/app.js
+++ pages/app.js
@@ -90,27 +90,50 @@ useEffect(() => {
     }
   }, [status]);
 
-  if (status === "loading" || showLoading) {
-    return (
-      <div className={styles["loader-container"]}>
-        <div className={styles.loader}>
-          <div>
-            <ul>
-              {[...Array(6)].map((_, i) => (
-                <li key={i}>
-                  <svg viewBox="0 0 90 120" fill="currentColor">
-                    <path d="M90,0 L90,120 L11,120 C4.92486775,120 0,115.075132 0,109 L0,11 C0,4.92486775 4.92486775,0 11,0 L90,0 Z M71.5,81 L18.5,81 C17.1192881,81 16,82.1192881 16,83.5 C16,84.8254834 17.0315359,85.9100387 18.3356243,85.9946823 L18.5,86 L71.5,86 C72.8807119,86 74,84.8807119 74,83.5 C74,82.1745166 72.9684641,81.0899613 71.6643757,81.0053177 L71.5,81 Z M71.5,57 L18.5,57 C17.1192881,57 16,58.1192881 16,59.5 C16,60.8254834 17.0315359,61.9100387 18.3356243,61.9946823 L18.5,62 L71.5,62 C72.8807119,62 74,60.8807119 74,59.5 C74,58.1192881 72.8807119,57 71.5,57 Z M71.5,33 L18.5,33 C17.1192881,33 16,34.1192881 16,35.5 C16,36.8254834 17.0315359,37.9100387 18.3356243,37.9946823 L18.5,38 L71.5,38 C72.8807119,38 74,36.8807119 74,35.5 C74,34.1192881 72.8807119,33 71.5,33 Z" />
-                  </svg>
-                </li>
-              ))}
-            </ul>
+  const Loader = ({ status, showLoading }) => {
+    const messages = [
+      "Pouring Coffee",
+      "Grabbing Your Notes",
+      "Setting Up Your Desk",
+      "Fetching Resources",
+      "Sharpening Your Pencils",
+      "Organizing Your Thoughts",
+      "Clearing Distractions",
+      "Boosting Your Focus"
+    ];
+  
+    const [messageIndex, setMessageIndex] = useState(0);
+  
+    useEffect(() => {
+      const interval = setInterval(() => {
+        setMessageIndex((prevIndex) => (prevIndex + 1) % messages.length);
+      }, 2000); // Change message every 2 seconds
+  
+      return () => clearInterval(interval); // Cleanup the interval on unmount
+    }, []);
+  
+    if (status === "loading" || showLoading) {
+      return (
+        <div className={styles["loader-container"]}>
+          <div className={styles.loader}>
+            <div>
+              <ul>
+                {[...Array(6)].map((_, i) => (
+                  <li key={i}>
+                    <svg viewBox="0 0 90 120" fill="currentColor">
+                      <path d="M90,0 L90,120 L11,120 C4.92486775,120 0,115.075132 0,109 L0,11 C0,4.92486775,0 11,0 L90,0 Z M71.5,81 L18.5,81 C17.1192881,81 16,82.1192881 16,83.5 C16,84.8254834 17.0315359,85.9100387 18.3356243,85.9946823 L18.5,86 L71.5,86 C72.8807119,86 74,84.8807119 74,83.5 C74,82.1745166 72.9684641,81.0899613 71.6643757,81.0053177 L71.5,81 Z M71.5,57 L18.5,57 C17.1192881,57 16,58.1192881 16,59.5 C16,60.8254834 17.0315359,61.9100387 18.3356243,61.9946823 L18.5,62 L71.5,62 C72.8807119,62 74,60.8807119 74,59.5 C74,58.1192881 72.8807119,57 71.5,57 Z M71.5,33 L18.5,33 C17.1192881,33 16,34.1192881 16,35.5 C16,36.8254834 17.0315359,37.9100387 18.3356243,37.9946823 L18.5,38 L71.5,38 C72.8807119,38 74,36.8807119 74,35.5 C74,34.1192881 72.8807119,33 71.5,33 Z" />
+                    </svg>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <span>Opening Your Study Session</span>
+            <span>{messages[messageIndex]}</span> {/* Dynamic line below */}
           </div>
-          <span>Opening Your Study Session</span>
         </div>
-      </div>
-    );
-  }
-
+      );
+    }
+  };
   if (status === "unauthenticated") {
     if (typeof window !== "undefined") {
       window.location.href = "/auth/signin";
