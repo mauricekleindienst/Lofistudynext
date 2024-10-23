3c5b0c98cb058ef21673ef37a4e38ca471f0f5e0
Maurice Kleindienst <support@mousewerk.de>
2024-10-23 09:33:38 +0200

    ssds

--- next.config.js
+++ next.config.js
@@ -1,5 +1,3 @@
-// next.config.js
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config) => {
@@ -8,26 +6,6 @@ const nextConfig = {
   },
   images: {
     domains: ['i.ibb.co'],
-  },
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: `
-              default-src 'self';
-              img-src 'self' https://i.ibb.co;
-              media-src 'self' https://i.ibb.co;
-              script-src 'self' 'unsafe-inline' 'unsafe-eval';
-              style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
-              font-src 'self' https://fonts.gstatic.com;
-            `,
-          },
-        ],
-      },
-    ];
   },
   env: {
     DATABASE_URL: process.env.DATABASE_URL,
--- pages/_document.js
+++ pages/_document.js
@@ -10,7 +10,45 @@ export default function Document() {
     author: {
       "@type": "Organization",
       name: "Lo-Fi.Study",
-      url: "https://www.lo-fi.study/",
+      url: "https://www.lo-fi.study/",// next.config.js
+
+      /** @type {import('next').NextConfig} */
+      const nextConfig = {
+        webpack: (config) => {
+          config.resolve.alias.canvas = false;
+          return config;
+        },
+        images: {
+          domains: ['i.ibb.co'],
+        },
+        async headers() {
+          return [
+            {
+              source: '/(.*)',
+              headers: [
+                {
+                  key: 'Content-Security-Policy',
+                  value: `
+                    default-src 'self';
+                    img-src 'self' https://i.ibb.co;
+                    media-src 'self' https://i.ibb.co;
+                    script-src 'self' 'unsafe-inline' 'unsafe-eval';
+                    style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
+                    font-src 'self' https://fonts.gstatic.com;
+                  `,
+                },
+              ],
+            },
+          ];
+        },
+        env: {
+          DATABASE_URL: process.env.DATABASE_URL,
+          // Add other environment variables you need here
+        },
+      };
+      
+      module.exports = nextConfig;
+      
     },
     description:
       "Lo-Fi.Study is a website that helps you study by providing a distraction-free environment. Enjoy ambient music and focus better.",
