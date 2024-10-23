712bebc2795913239345ccc947826080219f880e
Maurice Kleindienst <support@mousewerk.de>
2024-10-23 19:01:46 +0200

    sdsd

--- components/Footer.js
+++ components/Footer.js
@@ -22,8 +22,8 @@ export default function Footer() {
                         <li><Link href="/app">Study App</Link></li>
                         <li><Link href="/aboutus">About Us</Link></li>
                         <li><Link href="/dev-updates">Changelog</Link></li>
-                        <li><Link href="/faq">FAQ</Link></li>
-                        <li><Link href="/contact">Contact</Link></li>
+                        <li><Link href="/FAQ">FAQ</Link></li>
+                        <li><Link href="/Contact">Contact</Link></li>
                     </ul>
                 </div>
                 <div className={styles.navSection}>
